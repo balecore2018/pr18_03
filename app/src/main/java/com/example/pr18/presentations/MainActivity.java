6423b75917f8160bfed9a531863c277b28b99d5c
balecore <balecored@gmail.com>
2026-05-15 17:57:44 +0500

    Класс запроса разрешений Шаповалов

--- app/src/main/java/com/example/pr18/presentations/MainActivity.java
+++ app/src/main/java/com/example/pr18/presentations/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.pr18;
+package com.example.pr18.presentations;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.pr18.R;
 
 public class MainActivity extends AppCompatActivity {
 
